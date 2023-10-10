3665b9118db11c2507ccb6e30454b503413a1cf0
Patrick Schleizer <adrelanos@whonix.org>
2023-10-10 06:40:36 -0400

    wayland

--- usr/lib/python3/dist-packages/tor_control_panel/tor_control_panel.py
+++ usr/lib/python3/dist-packages/tor_control_panel/tor_control_panel.py
@@ -734,7 +734,7 @@ class TorControlPanel(QDialog):
         self.stop_tor()
         self.restart_button.setEnabled(False)
 
-        restart_command = 'systemctl --no-pager restart tor@default'
+        restart_command = 'pkexec systemctl --no-pager restart tor@default'
         p = Popen(restart_command, shell=True)
         self.start_bootstrap()
 
@@ -743,7 +743,7 @@ class TorControlPanel(QDialog):
         if not self.bootstrap_done:
             self.bootstrap_progress.hide()
             self.bootstrap_thread.terminate()
-        stop_command = 'systemctl --no-pager stop tor@default'
+        stop_command = 'pkexec systemctl --no-pager stop tor@default'
         p = Popen(stop_command, shell=True)
         p.wait()
         self.refresh(True)
