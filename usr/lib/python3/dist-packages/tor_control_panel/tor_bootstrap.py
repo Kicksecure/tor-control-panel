de7c35d9d245e0ce253b395d489b67b4443f8c0e
Patrick Schleizer <adrelanos@whonix.org>
2024-12-31 13:26:23 -0500

    copyright

--- usr/lib/python3/dist-packages/tor_control_panel/tor_bootstrap.py
+++ usr/lib/python3/dist-packages/tor_control_panel/tor_bootstrap.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3 -u
 # -*- coding: utf-8 -*-
 
-## Copyright (C) 2018 - 2023 ENCRYPTED SUPPORT LP <adrelanos@whonix.org>
+## Copyright (C) 2018 - 2025 ENCRYPTED SUPPORT LLC <adrelanos@whonix.org>
 ## See the file COPYING for copying conditions.
 
 import sys
